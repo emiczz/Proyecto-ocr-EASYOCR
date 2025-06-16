bc3fc66d593918e07653e7cd4314a62d5d1f8144
emiczz <emiliano1peralta@gmail.com>
2025-06-16 10:42:29 -0300

    Actualizacion

--- desenfoque_prueba.py
+++ desenfoque_prueba.py
@@ -3,7 +3,7 @@ import easyocr
 import numpy as np
 
 # 1) Cargar imagen original
-ruta_original = './pruebas/dni-prueba.webp'
+ruta_original = './pruebas/prueba-fotocopia.jpg'
 imagen = cv2.imread(ruta_original)
 
 #2) Inicializo EASYOCR
@@ -17,10 +17,12 @@ for x in resultado:
     
     print(f' Texto: "{x}')
     #Creo los puntos de trackeo (cuadro delimitador que envuelve el texto)
-    punto_cero = x[0][0]
-    punto_uno = x[0][1]
-    punto_dos = x[0][2]
-    punto_tres = x[0][3]
+    # Asegurarse de que sean tuplas de enteros
+    punto_cero = tuple(map(int, x[0][0]))
+    punto_uno = tuple(map(int, x[0][1]))
+    punto_dos = tuple(map(int, x[0][2]))
+    punto_tres = tuple(map(int, x[0][3]))
+
 
     cv2.rectangle(imagen,punto_cero,punto_dos,color=(160, 32, 240),thickness=4)
     
@@ -31,6 +33,7 @@ for x in resultado:
     cv2.circle(imagen,punto_uno, radius=2, color=(0,255,0), thickness=2)
     cv2.circle(imagen,punto_dos,radius=2, color=(0,0,255), thickness=2)
     cv2.circle(imagen,punto_tres,radius=2,color=(0,255,255),thickness=2)
+
 
 
 
